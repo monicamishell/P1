2fa47f789110b59355389dcec1063860bbbdc2d0
monicamishell <45182777+monicamishell@users.noreply.github.com>
2018-12-12 23:10:07 -0800

    Add files via upload

---
+++ Alumno.java
@@ -0,0 +1,28 @@
+class Alumno{
+	public static void main (String[] args){
+		String nombre= "Monica Michel Gonzaga Patino";
+		String universidad= "Universidad Autonoma de Baja California Campus Tijuana";
+		String cargo= "Estudiante";
+		String especialidad= "Computacion";
+		String telefono= "6647089139";
+		String direccion= "Santa Fe";
+		String pasatiempo= "Dibujar";
+
+		System.out.println("***   ***     ****     ******       *******   ");
+		System.out.println("***   ***    ******    ***   *     **      * ");
+		System.out.println("***   ***   **    **   ***   *    **         ");
+		System.out.println("***   ***  ***    ***  ***   *   **          ");
+		System.out.println("***   ***  **********  *******   **           ");
+		System.out.println("***   ***  **********  ***    *  **           ");
+		System.out.println("*********  ***    ***  ***    *   **          ");
+		System.out.println("*********  ***    ***  ***    *    **      *  ");
+		System.out.println(" *******   ***    ***  *******      *******   ");
+		System.out.println("\nNombre:"+nombre);
+		System.out.println("\nUniversidad:"+universidad);
+		System.out.println("\nCargo:"+cargo);
+		System.out.println("\nEspecialidad:"+especialidad);
+		System.out.println("\nTelefono:"+telefono);
+		System.out.println("\nDireccion:"+direccion);
+		System.out.println("\nPasatiempo:"+pasatiempo);
+	}
+}
